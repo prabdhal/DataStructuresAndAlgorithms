e06e23790fe8179497b7556c83e14972e394478b
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2021-02-14 23:32:51 -0500

    Create PrintPat() Method.

--- MathProblems.cs
+++ MathProblems.cs
@@ -1,9 +1,29 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace Mathematical 
+namespace Mathematical
 {
-    public class MathProblems
-    {
-        
-    }
+	class MathProblems
+	{
+		public static void PrintPat(int lengthOfPattern)
+		{
+			if (lengthOfPattern < 1 || lengthOfPattern > 40)
+			{
+				Console.WriteLine("lengthOfPattern value must be between 1 to 40");
+				return;
+			}
+
+			int temp = lengthOfPattern;
+			for (int i = lengthOfPattern; i > 0; i--)
+			{
+				temp -= lengthOfPattern - i;
+				for (int j = temp; j > 0; j--)
+				{
+					Console.WriteLine(temp);
+				}
+				Console.Write("");
+			}
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -5,273 +5,271 @@ using Mathematical;
 
 namespace DataStructuresAndAlgorithms
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-            Console.WriteLine("Please enter a word...");
-            string word = Console.ReadLine();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MathProblems.PrintPat(3);
+			Console.WriteLine("Please enter a word...");
+			string word = Console.ReadLine();
 
-            string reverseStringIgnoringSpecialChars = ReverseStringIgnoringSpecialChars(word);
-            string reverseEntireString = ReverseEntireString(word.ToCharArray());
-            string reverseWordOrder = ReverseWordOrder(word);
-            bool palindromeCheck = PalindromeCheck(word);
-            string reverseWords = ReverseWords(word);
-            string removeDuplicateOccurances = RemoveDuplicateOccurances(word);
+			string reverseStringIgnoringSpecialChars = ReverseStringIgnoringSpecialChars(word);
+			string reverseEntireString = ReverseEntireString(word.ToCharArray());
+			string reverseWordOrder = ReverseWordOrder(word);
+			bool palindromeCheck = PalindromeCheck(word);
+			string reverseWords = ReverseWords(word);
+			string removeDuplicateOccurances = RemoveDuplicateOccurances(word);
 
-            // Void Return-Type Methods
-            CharacterOccuranceCounter(word);
-            FindAllSubstrings(word);
+			// Void Return-Type Methods
+			CharacterOccuranceCounter(word);
+			FindAllSubstrings(word);
 
-            // Int Array Methods
-            int[] intAry = { 1, 2, 3, 4, 5 };
-            ShiftIntToLeft(intAry);
+			// Int Array Methods
+			int[] intAry = { 1, 2, 3, 4, 5 };
+			ShiftIntToLeft(intAry);
 
-            Console.Write("ReverseStringIgnoringSpecialChars - {0},\n" +
-                "ReverseEntireString - {1},\n" +
-                "ReverseWordOrder - {2},\n" +
-                "PalindromeCheck - {3},\n" +
-                "ReverseWords - {4},\n" +
-                "RemoveDuplicateOccurances - {5}\n",
-                reverseStringIgnoringSpecialChars, 
-                reverseEntireString, 
-                reverseWordOrder, 
-                palindromeCheck, 
-                reverseWords, 
-                removeDuplicateOccurances);
-        }
+			Console.Write("ReverseStringIgnoringSpecialChars - {0},\n" +
+				"ReverseEntireString - {1},\n" +
+				"ReverseWordOrder - {2},\n" +
+				"PalindromeCheck - {3},\n" +
+				"ReverseWords - {4},\n" +
+				"RemoveDuplicateOccurances - {5}\n",
+				reverseStringIgnoringSpecialChars, 
+				reverseEntireString, 
+				reverseWordOrder, 
+				palindromeCheck, 
+				reverseWords, 
+				removeDuplicateOccurances);
+		}
 
-        // Reverse entire string
-        public static string ReverseEntireString(string str)
-        {
-            string given = str;
-            string result = "";
+		// Reverse entire string
+		public static string ReverseEntireString(string str)
+		{
+			string given = str;
+			string result = "";
 
-            // reverse each letter, ignoring special characters
-            for (int i = given.Length - 1; i >= 0; i--)
-            {
-                // add char into temp string
-                result += given[i];
-            }
+			// reverse each letter, ignoring special characters
+			for (int i = given.Length - 1; i >= 0; i--)
+			{
+				// add char into temp string
+				result += given[i];
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        // Reverse entire string - Alternative
-        public static string ReverseEntireString(char[] str)
-        {
-            char[] given = str;
-            string result = "";
+		// Reverse entire string - Alternative
+		public static string ReverseEntireString(char[] str)
+		{
+			char[] given = str;
+			string result = "";
 
-            // Initialize right and left pointers
-            int r = given.Length - 1, l = 0;
+			// Initialize right and left pointers
+			int r = given.Length - 1, l = 0;
 
-            // Traverse string from both ends
-            while (l < r)
-            {
-                char temp = given[r];
-                given[r] = given[l];
-                given[l] = temp;
-                l++;
-                r--;
-            }
+			// Traverse string from both ends
+			while (l < r)
+			{
+				char temp = given[r];
+				given[r] = given[l];
+				given[l] = temp;
+				l++;
+				r--;
+			}
 
-            for (int i = 0; i < given.Length; i++)
-            {
-                result += given[i];
-            }
+			for (int i = 0; i < given.Length; i++)
+			{
+				result += given[i];
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        // Reverse a string without affecting special characters
-        //Ex. Input: str = "a,b$c"  output str = "c,b$a"
-        public static string ReverseStringIgnoringSpecialChars(string str)
-        {
-            char[] given = str.ToCharArray();
-            string result = "";
+		// Reverse a string without affecting special characters
+		//Ex. Input: str = "a,b$c"  output str = "c,b$a"
+		public static string ReverseStringIgnoringSpecialChars(string str)
+		{
+			char[] given = str.ToCharArray();
+			string result = "";
 
-            // Initialize left and right pointers
-            int r = given.Length - 1, l = 0;
+			// Initialize left and right pointers
+			int r = given.Length - 1, l = 0;
 
-            // Traverse string from both ends until
-            // 'l' and 'r'
-            while (l < r)
-            {
-                // ignore all special characters
-                if (!char.IsLetter(given[l]))
-                    l++;
-                else if (!char.IsLetter(given[r]))
-                    r--;
-                else
-                {
-                    char temp = given[l];
-                    given[l] = given[r];
-                    given[r] = temp;
-                    l++;
-                    r--;
-                }
-            }
+			// Traverse string from both ends until
+			// 'l' and 'r'
+			while (l < r)
+			{
+				// ignore all special characters
+				if (!char.IsLetter(given[l]))
+					l++;
+				else if (!char.IsLetter(given[r]))
+					r--;
+				else
+				{
+					char temp = given[l];
+					given[l] = given[r];
+					given[r] = temp;
+					l++;
+					r--;
+				}
+			}
 
-            for (int i = 0; i < given.Length; i++)
-            {
-                result += given[i];
-            }
+			for (int i = 0; i < given.Length; i++)
+			{
+				result += given[i];
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        // Reverse the word order
-        public static string ReverseWordOrder(string str)
-        {
-            string given = str;
-            string result = "";
+		// Reverse the word order
+		public static string ReverseWordOrder(string str)
+		{
+			string given = str;
+			string result = "";
 
-            string[] givenAry = given.Split(' ');
+			string[] givenAry = given.Split(' ');
 
-            for (int i = givenAry.Length - 1; i >= 0; i--)
-            {
-                result += givenAry[i];
-                if (i > 0) result += ' ';
-            }
+			for (int i = givenAry.Length - 1; i >= 0; i--)
+			{
+				result += givenAry[i];
+				if (i > 0) result += ' ';
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        // Indicate whether a string is Palindrome
-        public static bool PalindromeCheck(string str)
-        {
-            string given = str;
+		// Indicate whether a string is Palindrome
+		public static bool PalindromeCheck(string str)
+		{
+			string given = str;
 
-            int r = given.Length - 1, l = 0;
+			int r = given.Length - 1, l = 0;
 
-            while (l < r)
-            {
-                if (given[l] != given[r])
-                    return false;
+			while (l < r)
+			{
+				if (given[l] != given[r])
+					return false;
 
-                l++;
-                r--;
-            }
-            return true;
-        }
+				l++;
+				r--;
+			}
+			return true;
+		}
 
-        // Reverse only words of a string
-        public static string ReverseWords(string str)
-        {
-            string given = str;
-            string result = "";
+		// Reverse only words of a string
+		public static string ReverseWords(string str)
+		{
+			string given = str;
+			string result = "";
 
-            string[] givenAry = str.Split(' ');
+			string[] givenAry = str.Split(' ');
 
 
-            for (int i = 0; i < givenAry.Length; i++)
-            {
-                int r = givenAry[i].Length - 1, l = 0;
-                string word = givenAry[i];
+			for (int i = 0; i < givenAry.Length; i++)
+			{
+				string word = givenAry[i];
 
-                for (int j = word.Length - 1; j >= 0; j--)
-                {
-                    result += word.Substring(j, 1);
-                }
+				for (int j = word.Length - 1; j >= 0; j--)
+				{
+					result += word.Substring(j, 1);
+				}
 
-                if (i < givenAry.Length - 1)
-                    result += ' ';
+				if (i < givenAry.Length - 1)
+					result += ' ';
 
-                Console.WriteLine(result);
-            }
+				Console.WriteLine(result);
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        // Count the occurence of each character in a string
-        public static void CharacterOccuranceCounter(string str)
-        {
-            Dictionary<char, int> charCount = new Dictionary<char, int>();
+		// Count the occurence of each character in a string
+		public static void CharacterOccuranceCounter(string str)
+		{
+			Dictionary<char, int> charCount = new Dictionary<char, int>();
 
-            foreach (char c in str)
-            {
-                if (c != ' ')
-                {
-                    if (!charCount.ContainsKey(c))
-                    {
-                        charCount.Add(c, 1);
-                    }
-                    else
-                    {
-                        charCount[c]++;
-                    }
-                }
-            }
-            Console.WriteLine("-----Character Occurances of the string '{0}'-----", str);
-            foreach (var c in charCount)
-            {
-                Console.WriteLine("{0} occurances of the character {1}", c.Value, c.Key);
-            }
-            Console.WriteLine("-------------------------------");
-        }
+			foreach (char c in str)
+			{
+				if (c != ' ')
+				{
+					if (!charCount.ContainsKey(c))
+					{
+						charCount.Add(c, 1);
+					}
+					else
+					{
+						charCount[c]++;
+					}
+				}
+			}
+			Console.WriteLine("-----Character Occurances of the string '{0}'-----", str);
+			foreach (var c in charCount)
+			{
+				Console.WriteLine("{0} occurances of the character {1}", c.Value, c.Key);
+			}
+			Console.WriteLine("-------------------------------");
+		}
 
-        // Remove duplicate characters from a string
-        public static string RemoveDuplicateOccurances(string str)
-        {
-            string given = str;
-            string result = "";
+		// Remove duplicate characters from a string
+		public static string RemoveDuplicateOccurances(string str)
+		{
+			string given = str;
+			string result = "";
 
-            for (int i = 0; i < given.Length; i++)
-            {
-                if (result.Contains(given[i])) continue;
-                result += given[i];
-            }
+			for (int i = 0; i < given.Length; i++)
+			{
+				if (result.Contains(given[i])) continue;
+				result += given[i];
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        // Find all possible substring of a given string
-        public static void FindAllSubstrings(string str)
-        {
-            string given = str;
+		// Find all possible substring of a given string
+		public static void FindAllSubstrings(string str)
+		{
+			string given = str;
 
-            Console.WriteLine("------The substrings for '{0}'------", given);
-            // substring 1 value at a time all the way to length value
-            for (int i = 0; i < given.Length; i++)
-            {
-                for (int j = 1; j <= (given.Length - i); j++)
-                {
-                    Console.WriteLine("Substring - {0}", given.Substring(i, j));
-                }
-            }
-            Console.WriteLine("-------------------------------", given);
-        }
+			Console.WriteLine("------The substrings for '{0}'------", given);
+			// substring 1 value at a time all the way to length value
+			for (int i = 0; i < given.Length; i++)
+			{
+				for (int j = 1; j <= (given.Length - i); j++)
+				{
+					Console.WriteLine("Substring - {0}", given.Substring(i, j));
+				}
+			}
+			Console.WriteLine("-------------------------------", given);
+		}
 
-        // Perform a left circular rotation of an array (shift characters to the left by 1)
-        public static void ShiftIntToLeft(int[] numbers)
-        {
-            int[] given = numbers;
-            int[] result = new int[numbers.Length];
+		// Perform a left circular rotation of an array (shift characters to the left by 1)
+		public static void ShiftIntToLeft(int[] numbers)
+		{
+			int[] given = numbers;
+			int[] result = new int[numbers.Length];
 
-            // add all ints into result array starting from 2nd int
-            for (int i = 1; i < given.Length; i++)
-            {
-                result[i - 1] = given[i];
-            }
+			// add all ints into result array starting from 2nd int
+			for (int i = 1; i < given.Length; i++)
+			{
+				result[i - 1] = given[i];
+			}
 
-            // add 1st int to the end of the result array
-            result[given.Length - 1] = given[0];
+			// add 1st int to the end of the result array
+			result[given.Length - 1] = given[0];
 
-            Console.Write("------Shift the int array [");
-            foreach (var num in numbers)
-            {
-                Console.Write("{0}", num);
-            }
-            Console.Write("] to the left by one------\n");
-            Console.Write("The output is ");
-            foreach (var num in result)
-            {
-                Console.Write("{0}", num);
-            }
-            Console.WriteLine("\n-------------------------------", given);
-        }
-
-    }
+			Console.Write("------Shift the int array [");
+			foreach (var num in numbers)
+			{
+				Console.Write("{0}", num);
+			}
+			Console.Write("] to the left by one------\n");
+			Console.Write("The output is ");
+			foreach (var num in result)
+			{
+				Console.Write("{0}", num);
+			}
+			Console.WriteLine("\n-------------------------------", given);
+		}
+	}
 }
